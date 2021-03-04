e32f2d1ef5e68886810980a569375eb550e11eb3
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-03-04 08:30:10 +0000

    Initial commit - codeCademy - arrays and loops project.

---
+++ CaesarCipher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        }
+    }
+}
